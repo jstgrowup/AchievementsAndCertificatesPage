91acb716a0c2c88b4fee2fd7d914b378cb737962
jstgrowup <deysubham999@gmail.com>
2023-12-01 23:51:09 +0530

    fix

--- src/Picture.jsx
+++ src/Picture.jsx
@@ -6,6 +6,7 @@ function Picture({ src }) {
   return (
     <div data-aos="fade-up" data-aos-duration="2000">
       <Center>
+        
         <Image
           boxShadow="rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px"
           borderRadius="lg"
